d64c1638060bcd28145ef30b9068fa20a2d69034
MahimaNigam <98575235+MahimaNigam@users.noreply.github.com>
2022-02-03 11:15:55 +0530

    Update IssueForm.js
    Added more functionalities in the application.

--- IssueForm.js
+++ IssueForm.js
@@ -4,16 +4,15 @@ const IssueForm=(props)=>{
         <div id="issue-form">
         <form className="mt-5" onSubmit={props.addIssue}>
             <div className="form-row align-items-center">
-                <div className="col-lg-8">
+                <div className="col-lg-10">
                     <label htmlFor="taskName">Your Task</label>
                     <input type="text" placeholder="Start typing..."className="form-control" id="taskName"  value={props.currentIssue}
             onChange={props.updateIssue}/>
-                    <small id="taskHelp" className="form-text text-muted">Issue List</small>
+                     <small id="issueCompleted" className="form-text text-muted">Click on the issue to mark issue completion</small>
+                    <b id="taskHelp" className="form-text text-muted">Issue List </b>
                 </div>
-
-
                 <div className="col-auto">
-                    <button type="submit" className="btn btn-primary mt-2">Submit</button>
+                    <button type="submit" className="btn btn-primary mt-1 mb-4">Submit</button>
                 </div>
             </div>
         </form>
